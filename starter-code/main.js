41fefabe984ed57fd343acd85b0b7d3159c7a8b8
Aaron <andrews_aaron@bah.com>
2017-01-16 18:36:17 -0500

    created cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,6 +3,7 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+/*
 if (cardTwo === cardFour) {
 	alert ("You found a match!");
 } else if (cardOne === cardThree) {
@@ -12,3 +13,15 @@ if (cardTwo === cardFour) {
 } else if (CardTwo === cardThree){
 	alert ("Sorry, try again.")
 }
+*/
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++) {
+		var newDiv = document.createElement('div');
+		newDiv.id = 'game-board';
+		newDiv.className = 'card';
+		gameBoard.appendChild(newDiv);
+	}
+} 
+createCards();
+
